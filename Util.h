41024f3e1f86b2e111b38677951fb8f58918b8ce
LetzteInstanz <faust6@inbox.ru>
2023-12-28 23:08:50 +0300

    +const

--- Util.h
+++ Util.h
@@ -4,4 +4,4 @@ template <typename T>
 constexpr int to_int(T value) { return static_cast<int>(value); }
 
 template <typename T>
-constexpr T to_type(int value) { return static_cast<T>(value); }
+constexpr T to_type(const int value) { return static_cast<T>(value); }
