94d9afe0d175709a8058ec3f7dd7b5c05617f540
LetzteInstanz <faust6@inbox.ru>
2024-02-09 23:05:57 +0300

    Unused methods were dropped.

--- src/FileSystem/FileSystemObject.h
+++ src/FileSystem/FileSystemObject.h
@@ -13,15 +13,9 @@ public:
     FileSystemObject(QString&& name, const Type type, std::pair<Status, std::tm>&& last_modified_pair) noexcept;
 
     QString get_name() const noexcept { return _name; }
-    void set_name(const QString& name) noexcept { _name = name; }
     Type get_type() const noexcept { return _type; }
-    void set_type(Type type) noexcept { _type = type; }
-
     bool is_last_modified_valid() const noexcept { return _last_modified_pair.first == Status::Ok; }
-    //void set_last_modified(const std::tm& time) noexcept { _last_modified_pair.second = time; }
-
     std::tm get_last_modified() const noexcept { return _last_modified_pair.second; }
-    void set_last_modified(const std::tm& time) noexcept { _last_modified_pair.second = time; }
 
 private:
     QString _name;
